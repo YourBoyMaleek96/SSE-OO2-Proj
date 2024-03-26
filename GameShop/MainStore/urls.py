d356ac06382b8c64e813baffcfb7aa822c00f2fe
YourBoyMaleek96 <malikfreeman1996@gmail.com>
2024-03-26 19:01:59 -0400

    Converted to GUI
    - Started on queue implementation
    -Not quite right

--- GameShop/MainStore/urls.py
+++
@@ -1,9 +0,0 @@
-# Mainstore/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login, name='login'),
-    # Add other URL patterns as needed
-]
