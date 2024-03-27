177de034264931f6de5ecb40d966b8866fab95c6
YourBoyMaleek96 <malikfreeman1996@gmail.com>
2024-03-26 23:28:31 -0400

    *Had to redo GUI was not working on windows

---
+++ GUI.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Constant for color theme
+BLUE = "#1f6aa5"
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+        self.tail = new_node
+        self.size += 1
+
+    def dequeue(self):
+        if not self.head:
+            return None
+        value = self.head.value
+        self.head = self.head.next
+        if not self.head:
+            self.tail = None
+        self.size -= 1
+        return value
+
+class GameShopGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("GameShop")
+        master.attributes("-fullscreen", True)  # Set window to fit the user's screen
+
+        self.queue = Queue()
+        self.total_customers = 25
+        self.your_position = 10
+
+        # Create banner label spanning across the top
+        self.banner_label = tk.Label(master, text="Welcome to GameShop", bg=BLUE, fg="white", font=("Helvetica", 24))
+        self.banner_label.pack(fill=tk.X)
+
+        # Create access button
+        self.access_button = tk.Button(master, text="Access Website", bg=BLUE, fg="white", font=("Helvetica", 12), command=self.access_website)
+        self.access_button.pack(pady=50)
+
+    def access_website(self):
+        """This function is used to start queue"""
+        self.master.withdraw()
+
+        # Create a new window for the queue updates
+        queue_window = tk.Toplevel(self.master)
+        queue_window.title("GameShop")
+        queue_window.attributes("-fullscreen", True)  # Set window to fit the user's screen
+
+        def update_queue_label():
+            """This function is used to update the queue and keep track of position"""
+            if self.your_position > 2:
+                self.your_position -= 1
+                queue_label.config(text=f"You are {self.your_position}/{self.total_customers}. There are {self.your_position - 1} people in front of you.")
+                queue_window.after(2000, update_queue_label)  # Re-schedule the update
+            else:
+                queue_label.config(text="Welcome to GameShop")
+
+        # Create label to display queue status spanning across the top
+        queue_label = tk.Label(queue_window, text=f"You are {self.your_position}/{self.total_customers}. There are {self.your_position - 1} people in front of you.", font=("Helvetica", 12), bg=BLUE, fg="white")
+        queue_label.pack(fill=tk.X, pady=(50, 0))
+
+        # Start updating the queue label and countdown
+        update_queue_label()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = GameShopGUI(root)
+    root.mainloop()
